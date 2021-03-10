366be53cb52b3cf0ed45dde47ecce30093a53521
Milica Pajic <micy_98@live.com>
2021-03-10 19:08:31 +0100

    Initial commit

---
+++ UniversityWebService/src/main/java/com/uni/UniversityWebService/UniversityWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uni.UniversityWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityWebServiceApplication.class, args);
+	}
+
+}
---
+++ UniversityWebService/src/test/java/com/uni/UniversityWebService/UniversityWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.UniversityWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
