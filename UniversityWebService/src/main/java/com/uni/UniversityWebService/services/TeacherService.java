54ed0c530f8afb135e26912dc3c0094e29513063
Milica Pajic <micy_98@live.com>
2021-03-23 04:07:48 +0100

    Added methods for returning list of exam parts.
    closes #6

--- UniversityWebService/src/main/java/com/uni/UniversityWebService/services/TeacherService.java
+++ UniversityWebService/src/main/java/com/uni/UniversityWebService/services/TeacherService.java
@@ -7,8 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.uni.UniversityWebService.model.CourseInstance;
+import com.uni.UniversityWebService.model.Enrollment;
+import com.uni.UniversityWebService.model.Exam;
+import com.uni.UniversityWebService.model.ExamPart;
+import com.uni.UniversityWebService.model.ExamPeriod;
 import com.uni.UniversityWebService.model.Teacher;
 import com.uni.UniversityWebService.model.Teaching;
+import com.uni.UniversityWebService.repositories.ExamPartRepository;
 import com.uni.UniversityWebService.repositories.TeacherRepository;
 
 @Service
@@ -16,6 +21,9 @@ public class TeacherService {
 	
 	@Autowired
 	private TeacherRepository teacherRepository;
+	
+	@Autowired
+	private ExamPartRepository examPartRepository;
 	
 	public List<CourseInstance> findTeacherCourses(Long id){
 		
@@ -40,5 +48,24 @@ public class TeacherService {
 
 	public Teacher save(Teacher teacher) {
 		return teacherRepository.save(teacher);
+	}
+	
+	/*
+	 * public List <ExamPart> findTeacherExams(Long id)
+	 * { Teacher teacher =teacherRepository.findById(id).get();
+	 *  List<ExamPart> examParts = new ArrayList<>(); 
+	 *  for(Teaching t : teacher.getTeachings())
+	 *   { for(Enrollment e : t.getCourseInstance().getEnrollments())
+	 *    { for(ExamPeriod ep : e.getExamPeriods()) 
+	 *    { for(Exam exam : ep.getExams()) 
+	 *    { for(ExamPart examPa :exam.getExamParts()) 
+	 *    { examParts.add(examPa); } } } } } 
+	 *    return examParts;
+	 * 
+	 * }
+	 */
+	public List<ExamPart> findExamParts(){
+		List<ExamPart> examParts =examPartRepository.findAll();
+		return examParts;
 	}
 }
