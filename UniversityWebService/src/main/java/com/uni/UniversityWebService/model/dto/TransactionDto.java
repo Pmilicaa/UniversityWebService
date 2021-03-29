f8b918e8a040fb9a5c2f2322b15054a05e74f3c8
Mratinkovic <dusanm242@gmail.com>
2021-03-30 01:20:03 +0200

    TransactionDto reworked

--- UniversityWebService/src/main/java/com/uni/UniversityWebService/model/dto/TransactionDto.java
+++ UniversityWebService/src/main/java/com/uni/UniversityWebService/model/dto/TransactionDto.java
@@ -2,15 +2,7 @@ package com.uni.UniversityWebService.model.dto;
 
 import com.uni.UniversityWebService.model.Student;
 import com.uni.UniversityWebService.model.TransactionType;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 
 public class TransactionDto {
 
@@ -20,7 +12,42 @@ public class TransactionDto {
 		private TransactionType transactionType;
 		private Long studentId;
 
+	public TransactionDto(Long id, Integer amount, TransactionType transactionType, Long studentId) {
+		this.id = id;
+		this.amount = amount;
+		this.transactionType = transactionType;
+		this.studentId = studentId;
+	}
 
+	public void setStudentId(Long studentId) {
+		this.studentId = studentId;
+	}
 
+	public void setTransactionType(TransactionType transactionType) {
+		this.transactionType = transactionType;
+	}
 
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getStudentId() {
+		return studentId;
+	}
+
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public TransactionType getTransactionType() {
+		return transactionType;
+	}
 }
