b1d00c44e4cb4c08e01c632d58126fbb24b81f8b
aleksap99 <aleksapetrovic99@gmail.com>
2021-03-30 02:30:59 +0200

    Added UserDto closes #21

---
+++ UniversityWebService/src/main/java/com/uni/UniversityWebService/model/dto/UserDto.java
@@ -0,0 +1,41 @@
+package com.uni.UniversityWebService.model.dto;
+
+import com.uni.UniversityWebService.model.Role;
+import com.uni.UniversityWebService.model.User;
+
+public class UserDto {
+
+    private long id;
+    private String username;
+    private Role role;
+
+    public UserDto(User user){
+        this.id = user.getId();
+        this.username = user.getUserName();
+        this.role = user.getRole();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
