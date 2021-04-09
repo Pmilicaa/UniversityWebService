7d98cf0abde19a5b6c9f2c869ab6a6ebe83d4407
Damir <Damir@DESKTOP-1BP0HST>
2021-04-09 03:47:07 +0200

    [UPDATE] In StudentController added two methods
    for showing student remaining exams

--- UniversityWebService/src/main/java/com/uni/UniversityWebService/controllers/StudentController.java
+++ UniversityWebService/src/main/java/com/uni/UniversityWebService/controllers/StudentController.java
@@ -1,8 +1,18 @@
 package com.uni.UniversityWebService.controllers;
 
+import com.uni.UniversityWebService.model.CourseSpecification;
+import com.uni.UniversityWebService.model.Enrollment;
 import com.uni.UniversityWebService.model.Role;
 import com.uni.UniversityWebService.model.Student;
+import com.uni.UniversityWebService.model.dto.CourseSpecificationDto;
+import com.uni.UniversityWebService.model.dto.EnrollmentDto;
+import com.uni.UniversityWebService.services.CourseService;
 import com.uni.UniversityWebService.services.StudentService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +23,45 @@ public class StudentController {
 
     @Autowired
     private StudentService studentService;
+    
 
     @GetMapping(path = "/students")
     public @ResponseBody
     ResponseEntity<?> getAllStudents(){
         return new ResponseEntity(studentService.findAllStudents(), HttpStatus.OK);
     }
+    
+    
+    //This is my way how to show unpassed exams with information of the courses
+    @GetMapping(path = "students/{studentId}/courses")
+    public ResponseEntity<List<CourseSpecificationDto>> getStudentRemainingExams(@PathVariable("studentId") Long studentId) {
+    	List<CourseSpecification> studentRemainingExams = studentService.getRemainingExamsByStudent(studentId);
+    	List<CourseSpecificationDto> courseSpecificationsDto = new ArrayList<CourseSpecificationDto>();
+    	for(CourseSpecification cs : studentRemainingExams) {
+    		courseSpecificationsDto.add(new CourseSpecificationDto(cs));
+    	}
+		return new ResponseEntity<List<CourseSpecificationDto>>(courseSpecificationsDto, HttpStatus.OK);
+    }
+    
+    
+    //This is teachers example how to show student courses
+    @GetMapping(path = "students/{studentId}/courses")
+    public ResponseEntity<List<EnrollmentDto>> getStudentCourses(
+			@PathVariable("studentId") Long studentId) {
+		Student student = studentService.findByOne(studentId);
+		List<Enrollment> enrollments = student.getEnrollments();
+		List<EnrollmentDto> enrollmentsDTO = new ArrayList<>();
+		for (Enrollment e: enrollments) {
+			EnrollmentDto enrollmentDTO = new EnrollmentDto();
+			enrollmentDTO.setId(e.getId());
+			enrollmentDTO.setCourseSpecificationDto(new CourseSpecificationDto(e.getCourseSpecification()));
+			//we leave student and exam fields empty
+			
+			enrollmentsDTO.add(enrollmentDTO);
+		}
+		return new ResponseEntity<List<EnrollmentDto>>(enrollmentsDTO, HttpStatus.OK);
+	}
+    
 
     @PostMapping(path = "/students")
     public @ResponseBody ResponseEntity<?> addStudent(@RequestBody Student student){
