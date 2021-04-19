abc8bed79e5e0c4a2114970f78104e10e30f53a6
aleksap99 <aleksapetrovic99@gmail.com>
2021-04-19 23:41:49 +0200

    Added transaction table component

--- FrontendWebService/src/app/app.module.ts
+++ FrontendWebService/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ListGroupComponent } from './list-group/list-group.component';
 import { DocumentTableComponent } from './document-table/document-table.component';
 import { StudentInfoComponent } from './student-info/student-info.component';
 import { ProfessorInfoComponent } from './professor-info/professor-info.component';
+import { FinanceTableComponent } from './finance-table/finance-table.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { ProfessorInfoComponent } from './professor-info/professor-info.componen
     ListGroupComponent,
     DocumentTableComponent,
     StudentInfoComponent,
-    ProfessorInfoComponent
+    ProfessorInfoComponent,
+    FinanceTableComponent
   ],
   imports: [
     BrowserModule
