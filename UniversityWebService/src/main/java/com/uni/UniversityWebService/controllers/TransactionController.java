e2e1a9ed65b5046e4577b3dd7cd8a1a610d17de3
Aleksa Petrovic <aleksapetrovic99@gmail.com>
2021-05-11 00:18:49 +0200

    Transaction controller, transaction, transaction service

--- UniversityWebService/src/main/java/com/uni/UniversityWebService/controllers/TransactionController.java
+++ UniversityWebService/src/main/java/com/uni/UniversityWebService/controllers/TransactionController.java
@@ -4,22 +4,24 @@ import com.uni.UniversityWebService.model.dto.TransactionDto;
 import com.uni.UniversityWebService.services.TransactionService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/transaction")
 public class TransactionController {
 
-			@Autowired
-            private  TransactionService transactionService;
+    @Autowired
+    private  TransactionService transactionService;
 
-            @PostMapping("/add")
-            public ResponseEntity<String> addNewTransaction(@RequestBody TransactionDto transactionDto) throws Exception {
-                transactionService.addNewTransaction(transactionDto);
-                return ResponseEntity.ok("Successful");
-            }
+    @PostMapping("/transactions")
+    public ResponseEntity<String> addNewTransaction(@RequestBody TransactionDto transactionDto) throws Exception {
+        transactionService.addNewTransaction(transactionDto);
+        return ResponseEntity.ok("Successful");
+    }
+
+    @GetMapping("/transactions")
+    public ResponseEntity<?> getAllTransactions(){
+        return new ResponseEntity(transactionService.findAll(), HttpStatus.OK);
+    }
 }
