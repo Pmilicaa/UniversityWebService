60b6f3dc2dd32d9945160b187fd27620084b0bf8
Damir Tizmonar <you@example.com>
2021-05-10 22:58:13 +0200

    [ADDED] User and Students in importSql

--- UniversityWebService/src/main/resources/import.sql
+++ UniversityWebService/src/main/resources/import.sql
@@ -6,3 +6,16 @@ insert into document values (1, "Diploma", "asdasdasd", 1);
 insert into document values (2, "Diploma", "asdasdasd", 2);
 insert into document values (3, "Diploma", "asdasdasd", 2);
 insert into document values (4, "Diploma", "asdasdasd", 1);
+
+--User
+insert into user values (1, "papa", 0, "paki");
+insert into user values (2, "pele", 1, "peki");
+insert into user values (3, "pale", 2, "pal");
+insert into user values (4, "neko", 2, "neko");
+
+
+--Students
+insert into student values (1, 374387438, 1000, "SF-9-2018", "Aleksa", "Petrovic", 1);
+insert into student values (2, 327827387, 2000, "SF-11-2018", "Milica", "Pajic", 2);
+insert into student values (3, 273283783, 1000, "SF-12-2018", "Damir", "Tizmonar", 3);
+insert into student values (4, 545544545, 1000, "SF-2-2018", "Neko", "Nekic", 4);
