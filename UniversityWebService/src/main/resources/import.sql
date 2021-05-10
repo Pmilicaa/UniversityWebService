df6b3c2cba16a73212aa47d0a6c39b8503bb1683
Aleksa Petrovic <aleksapetrovic99@gmail.com>
2021-05-10 02:21:45 +0200

    Added import sql file

---
+++ UniversityWebService/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+insert into document_type values (1, "D", "Diploma");
+insert into document_type values (2, "N", "Nesto");
+insert into document_type values (3, "S", "Sluzba");
+
+insert into document values (1, "Diploma", "asdasdasd", 1);
+insert into document values (2, "Diploma", "asdasdasd", 2);
+insert into document values (3, "Diploma", "asdasdasd", 2);
+insert into document values (4, "Diploma", "asdasdasd", 1);
