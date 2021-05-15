70fa2835a0a62abaf0acf774d5f19ac2e5923ea5
Mratinkovic <dusanm242@gmail.com>
2021-05-15 21:18:42 +0200

    Exam component fixed

--- FrontendWebService/src/app/exam/exam.component.ts
+++ FrontendWebService/src/app/exam/exam.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {Exam} from "../models/Exam";
+import {Course} from "../models/Course";
 @Component({
   selector: 'app-exam',
   templateUrl: './exam.component.html',
   styleUrls: ['./exam.component.css']
 })
 export class ExamComponent implements OnInit {
-  exam:Exam={
-    id:142,
-    name:'Internet Mreze',
-    dateOfregistration:'05/14/2021',
-    points:100,
-    grade:10
-    
-  }
-  constructor() { }
+  exams:Exam[]=[
+    {id:1,name:'Web programiranje',dateOfregistration:'2020-02-02', points: 100, grade:10},
+    {id:2,name: 'Baza podataka' ,dateOfregistration:'2020-04-02', points: 99, grade:10},
+    {id:3,name:'Internet mreze',dateOfregistration:'2020-06-02', points: 100, grade:10},
+    {id:4,name: 'Asembler',dateOfregistration:'2020-07-02', points: 100, grade:10}
+  ];
+  courses:Course[]=[
+    {id:'1',title:'Matematika 1',ECTS:8,code:'SIT02D',teacher:'Gabrijela Grujic'},
+    {id:'2',title:'Matematika 2',ECTS:8,code:'SIT08',teacher:'Mile Moler'}
+
+
+  ];
+  
 
   ngOnInit(): void {
   }
---
+++ FrontendWebService/src/app/models/Course.ts
@@ -0,0 +1,8 @@
+export class Course{
+    id:string;
+    title:string;
+    ECTS:number;
+    code: string;
+    teacher:string;
+}
+//Ovo je samo napravljeno kao dummy klasa,u bekendu jos ima polja enrollment i course instace
