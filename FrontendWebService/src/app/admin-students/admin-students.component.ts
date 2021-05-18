ee44316031152a0847f679b13cc9aa610eec7a49
Damir Tizmonar <you@example.com>
2021-05-18 09:40:30 +0200

    [UPDATED] in student table show forms on click button

--- FrontendWebService/src/app/admin-students/admin-students.component.ts
+++ FrontendWebService/src/app/admin-students/admin-students.component.ts
@@ -11,12 +11,44 @@ export class AdminStudentsComponent implements OnInit {
 
   students: Student[];
 
-  constructor(private adminStudentService: AdminStudentsService) { }
+  student: Student
+
+  constructor(private adminStudentService: AdminStudentsService) { 
+    // this.student = new Student ({
+    //   firstName: '',
+    //   lastName: '',
+    //   cardNumber: '',
+    //   balance: 0,
+    //   accountNumber: 0
+    // });
+  }
 
   title = "Students"
 
   ngOnInit(): void {
     this.adminStudentService.getAll().subscribe((students) => (this.students = students))
   }
+
+  public showAddStudent(): void {
+    let x = document.getElementById("addStudent");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  public showEditStudent(): void {
+    let x = document.getElementById("editStudent");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  // public add(): void {
+  //   this.adminStudentService.saveStudent(this.student);
+  // }
 
 }
