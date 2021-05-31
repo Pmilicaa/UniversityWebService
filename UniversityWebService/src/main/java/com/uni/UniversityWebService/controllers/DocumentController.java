76d4d3375b8888e46602aebf10d8e06f815b423f
Aleksa Petrovic <aleksapetrovic99@gmail.com>
2021-05-31 17:47:15 +0200

    Added document uploading and downloading on backend

--- UniversityWebService/src/main/java/com/uni/UniversityWebService/controllers/DocumentController.java
+++ UniversityWebService/src/main/java/com/uni/UniversityWebService/controllers/DocumentController.java
@@ -1,12 +1,21 @@
 package com.uni.UniversityWebService.controllers;
 
+import com.uni.UniversityWebService.model.FileResponse;
 import com.uni.UniversityWebService.services.DocumentService;
+import com.uni.UniversityWebService.services.FileSystemStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class DocumentController {
@@ -19,4 +28,58 @@ public class DocumentController {
         return new ResponseEntity(documentService.findAll(), HttpStatus.OK);
     }
 
+    @Autowired
+    private FileSystemStorageService storageService;
+
+
+    @GetMapping("/")
+    public String listAllFiles(Model model) {
+
+        model.addAttribute("files", storageService.loadAll().map(
+                path -> ServletUriComponentsBuilder.fromCurrentContextPath()
+                        .path("/download/")
+                        .path(path.getFileName().toString())
+                        .toUriString())
+                .collect(Collectors.toList()));
+
+        return "listFiles";
+    }
+
+    @GetMapping("/download/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
+
+        try{
+            Resource resource = storageService.loadAsResource(filename);
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION,
+                            "attachment; filename=\"" + resource.getFilename() + "\"")
+                    .body(resource);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return new ResponseEntity("File not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/upload-file")
+    @ResponseBody
+    public FileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+        String name = storageService.store(file);
+
+        String uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/download/")
+                .path(name)
+                .toUriString();
+
+        return new FileResponse(name, uri, file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/upload-multiple-files")
+    @ResponseBody
+    public List<FileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        return Arrays.stream(files)
+                .map(file -> uploadFile(file))
+                .collect(Collectors.toList());
+    }
 }
