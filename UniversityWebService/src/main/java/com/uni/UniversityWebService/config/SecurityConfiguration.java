bec6fbef639173807af53b210408f768fbb4b46c
Aleksa Petrovic <aleksapetrovic99@gmail.com>
2021-05-31 14:53:34 -0700

    Fixed accidental delete

---
+++ UniversityWebService/src/main/java/com/uni/UniversityWebService/config/SecurityConfiguration.java
@@ -0,0 +1,80 @@
+package com.uni.UniversityWebService.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.uni.UniversityWebService.security.AuthenticationTokenFilter;
+import com.uni.UniversityWebService.services.UserDetailsServiceImpl;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    public void configureAuthentication(
+            AuthenticationManagerBuilder authenticationManagerBuilder)
+            throws Exception {
+
+        authenticationManagerBuilder
+                .userDetailsService(this.userDetailsService).passwordEncoder(
+                passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public AuthenticationTokenFilter authenticationTokenFilterBean()
+            throws Exception {
+        AuthenticationTokenFilter authenticationTokenFilter = new AuthenticationTokenFilter();
+        authenticationTokenFilter
+                .setAuthenticationManager(authenticationManagerBean());
+        return authenticationTokenFilter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/api/login", "/styles.css", "/profile",
+                        "/runtime.js", "/polyfills.js", "/vendor.js", "/main.js", "/images/**", "/js/**",
+                        "/runtime-es2015.js", "/polyfills-es2015.js", "/vendor-es2015.js", "/main-es2015.js").permitAll()
+                .antMatchers(HttpMethod.POST,"/users").permitAll()
+                .antMatchers("/students/me").hasRole("STUDENT")
+                .antMatchers(HttpMethod.POST, "/api/**")
+                .hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+
+        // Custom JWT based authentication
+        httpSecurity.addFilterBefore(authenticationTokenFilterBean(),
+                UsernamePasswordAuthenticationFilter.class);
+    }
+}
