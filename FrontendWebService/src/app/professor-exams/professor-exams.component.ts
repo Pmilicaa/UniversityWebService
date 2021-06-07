57b61f3cbc949d76b5bacc37dd4eeac8c6ea3b30
Milica Pajic <micy_98@live.com>
2021-06-08 01:35:49 +0200

    Added professor-exams component

---
+++ FrontendWebService/src/app/professor-exams/professor-exams.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-professor-exams',
+  templateUrl: './professor-exams.component.html',
+  styleUrls: ['./professor-exams.component.css']
+})
+export class ProfessorExamsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
