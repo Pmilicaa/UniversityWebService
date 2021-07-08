c9cccf0926492fb5091c66ba88738fde6cb3a953
Mratinkovic <dusanm242@gmail.com>
2021-07-08 21:09:01 +0200

    Fix

--- UniversityWebService/src/main/java/com/uni/UniversityWebService/model/ExamPart.java
+++ UniversityWebService/src/main/java/com/uni/UniversityWebService/model/ExamPart.java
@@ -31,8 +31,6 @@ public class ExamPart {
 	@JoinColumn(name = "examPartTypeId", referencedColumnName = "examPartTypeId", nullable = false)
 	private ExamPartType examPartType;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	private Exam exam;
 
 
 	@ManyToOne
