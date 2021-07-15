9726840fcdba3af29a2ccb325284dea539112b0b
Milica Pajic <micy_98@live.com>
2021-07-15 05:01:14 +0200

    Added tables and methods for professor exam parts

--- FrontendWebService/src/app/services/professor-service.service.ts
+++ FrontendWebService/src/app/services/professor-service.service.ts
@@ -4,6 +4,9 @@ import { Professor } from '../models/Professor';
 import { Observable } from 'rxjs';
 import { AuthenticationServiceService } from './authentication-service.service';
 import ExamPartsAndCourses from '../models/ExamPartsAndCourses';
+import { Student } from '../models/Student';
+import { Exam } from '../models/Exam';
+import ExamPart from '../models/ExamPart';
 
 
 @Injectable({
@@ -27,6 +30,19 @@ export class ProfessorServiceService {
     const headers = new HttpHeaders({"Content-Type": "application/json", "X-Auth-Token": this.authService.getToken().toString()});
     const url = `${this.path}/${professor.id}/examPartsAndSpec/${period}`;
     return this.http.get<ExamPartsAndCourses[]>(url, {headers: headers});
+  }
+  getRegisteredStudent(course : String): Observable<Student[]>{
+    const headers = new HttpHeaders({"Content-Type": "application/json", "X-Auth-Token": this.authService.getToken().toString()});
+    return this.http.get<Student[]>("http://localhost:8080/teachers/registeredStudentsForExam/" + course, {headers: headers});
+
+  }
+  getExamPartStudentForGrade(id : number): Observable<ExamPart[]>{
+    const headers = new HttpHeaders({"Content-Type": "application/json", "X-Auth-Token": this.authService.getToken().toString()});
+    return this.http.get<ExamPart[]>("http://localhost:8080/teachers/student/" + id, {headers: headers});
+  }
+  getExamPartWithPoints(id : number, inputValue : string): Observable<ExamPart>{
+    const headers = new HttpHeaders({"Content-Type": "application/json", "X-Auth-Token": this.authService.getToken().toString()});
+    return this.http.get<ExamPart>("http://localhost:8080/teachers/examParts/" + id + "/" + inputValue, {headers: headers});
   }
 }
 
